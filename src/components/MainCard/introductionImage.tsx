bb21139aa6cb7ca941c84a52ceb1eb6d357c0a19
dinhhoangtung <67038957+dinhhoangtung@users.noreply.github.com>
2024-06-04 16:37:54 +0700

    Part 13

--- src/components/MainCard/introductionImage.tsx
+++ src/components/MainCard/introductionImage.tsx
@@ -1,64 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 
 const images = [
-  {
-    src: "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2931.jpg",
-  },
-  {
-    src: "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2923.jpg",
-  },
-  {
-    src: "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2952.jpg",
-  },
-  {
-    src: "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2913.jpg",
-  },
-  {
-    src: "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2911.jpg",
-  },
+  "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2931.jpg",
+
+  "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2923.jpg",
+
+  "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2952.jpg",
+
+  "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2913.jpg",
+
+  "https://phongdl.uit.edu.vn/sites/phongdl/files/slider/duc-2911.jpg",
 ];
 
-const Slideshow: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const IntroImage = () => {
+  const [activeImage, setActiveImage] = useState(0);
+  const handleClickNext = () => {
+    const nextImage = activeImage + 1;
+    if (nextImage >= images.length) {
+      setActiveImage(0);
+    } else {
+      setActiveImage(activeImage);
+    }
+  };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 2000);
-    return () => clearInterval(interval);
-  }, []);
+  const handleClickPrevios = () => {
+    const nextImage = activeImage - 1;
+    if (nextImage < 0) {
+      setActiveImage(images.length - 1);
+    } else {
+      setActiveImage(activeImage);
+    }
+  };
 
   return (
-    <div className="flex flex-col  content-center min-h-full bg-gray-100">
-      <div className="relative w-full max-w-2xl ">
-        {images.map((image, index) => (
+    <div>
+      <div>
+        {images.map((item, index) => (
           <div
             key={index}
-            className={`mySlides fade absolute inset-0 transition-opacity duration-1000 ${
-              index === currentIndex ? "opacity-100" : "opacity-0"
-            }`}
+            className='absolute opacity-${activeImageID === images.id ? "100" : "0"}'
           >
-            <img
-              src={image.src}
-              alt={`Slide ${index + 1}`}
-              className="w-full h-auto rounded-lg"
-            />
-            <div className="flex justify-center absolute bottom-[0%] mt-4">
-              {images.map((_, index) => (
-                <span
-                  key={index}
-                  className={`dot h-3 w-3 mx-1 rounded-full cursor-pointer ${
-                    index === currentIndex ? "bg-blue-500" : "bg-gray-300"
-                  }`}
-                  onClick={() => setCurrentIndex(index)}
-                ></span>
-              ))}
-            </div>
+            <img src={item} alt="" /> {""}
           </div>
         ))}
+      </div>
+      <div
+        className="absolute top-[50%] translate-y-[50%] text-black right-0"
+        onClick={handleClickNext}
+      >
+        <GoChevronRight />
+      </div>
+      <div
+        className="absolute top-[50%] translate-y-[50%] text-black left-0"
+        onClick={handleClickPrevios}
+      >
+        <GoChevronLeft />
       </div>
     </div>
   );
 };
 
-export default Slideshow;
+export default IntroImage;
